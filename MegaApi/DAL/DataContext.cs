c70c936f42bbc12663141257b78d36cd5097bb72
poproshaikin <150174304+poproshaikin@users.noreply.github.com>
2024-04-04 11:23:27 +0200

    Repositories refactoring

--- MegaApi/DAL/DataContext.cs
+++ MegaApi/DAL/DataContext.cs
@@ -1,6 +1,43 @@
-﻿namespace MegaApi.DAL;
+﻿using MegaApi.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class DataContext
+namespace MegaApi.DAL;
+
+public class DataContext : DbContext
 {
+    public DbSet<Image>   Images   { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Vendor>  Vendors  { get; set; }
+
+    private IConfiguration _config;
     
+    private string _connectionString => _config.GetConnectionString("Postgres")!;
+
+    public DataContext(IConfiguration config)
+    {
+        _config = config;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasOne<Image>(p => p.Image)
+            .WithOne()
+            .HasForeignKey<Product>(p => p.ImageId);
+
+        modelBuilder.Entity<Vendor>()
+            .HasMany<Product>()
+            .WithOne()
+            .HasForeignKey(p => p.VendorId);
+
+        modelBuilder.Entity<Vendor>()
+            .HasOne<Image>(v => v.Image)
+            .WithOne()
+            .HasForeignKey<Vendor>(v => v.ImageId);
+    }
 }
